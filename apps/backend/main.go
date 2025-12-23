bfc68cbfd87a929461ac34731065eb0759c1e4cf
Sadra Samadi <sadra.smd@gmail.com>
2025-12-23 22:54:37 +0330

    added database

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
-	"net/http"
-
+	"context"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"net/http"
+	"os"
 )
+
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"items": [...]map[string]any{
-				{"id": 1, "title": "Item 1"},
-				{"id": 2, "title": "Item 2"},
-				{"id": 3, "title": "Item 3"},
-			},
-		})
+		items, err := GetItems()
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{"items": items})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
 	})
 	_ = r.Run()
+}
+
+func GetItems() ([]Item, error) {
+	url := os.Getenv("DB_URL")
+	ctx := context.Background()
+	db, err := pgx.Connect(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close(ctx)
+	rows, err := db.Query(ctx, "select id, name from items")
+	if err != nil {
+		return nil, err
+	}
+	var items []Item
+	for rows.Next() {
+		item := Item{}
+		err = rows.Scan(&item.ID, &item.Name)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+	return items, rows.Err()
 }
